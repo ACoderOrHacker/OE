927fbc91c7bac86bd0408dbdba2747a1c64460a6
ACoderOrHacker <sgy2788@163.com>
2026-06-27 15:58:18 +0800

    feat(xmake.lua): update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -93,8 +93,6 @@ target("oed")
     end
 
     add_headerfiles("source/(**.d)")
-    add_rules("utils.install.cmake_importfiles")
-    add_rules("utils.install.pkgconfig_importfiles")
 target_end()
 
 --- tests and benchmarks ---
@@ -106,10 +104,12 @@ option("tests")
 
         add_files("tests/test.cpp")
         add_deps("oecpp")
+    target_end()
 
     target("test-d")
         set_kind("binary")
 
         add_files("tests/test.d")
         add_deps("oed")
+    target_end()
 option_end()
