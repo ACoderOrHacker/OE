49b2cdcd398ef150f7e02167aee259d72b68605e
ACoderOrHacker <ACoderOrHacker@zohomail.com>
2026-07-21 19:14:30 +0800

    test(types): add tests for types

---
+++ tests/base_test.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file base_test.hpp
+ * @brief Tests utilities, based on GoogleTest and Google Benchmark, only
+ * supports C++.
+ * @copyright Copyright (C) 2026 ACoderOrHacker. All rights reserved.
+ */
+#ifndef OE_TESTS_BASE_TEST_HPP
+#define OE_TESTS_BASE_TEST_HPP
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <benchmark/benchmark.h>
+
+#endif // !OE_TESTS_BASE_TEST_HPP
